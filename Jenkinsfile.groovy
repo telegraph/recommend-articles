425b974240c3a107a3b9ab2eed0afe3ff279be11
Ather Mughal <ather.mughal@telegraph.co.uk>
2018-09-21 13:55:37 +0100

    Enabled prod deployment stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -49,7 +49,7 @@ ansiColor('xterm') {
             }
 
             stage("Build & Unit Tests"){
-              sendNotification("Build", "${env.SLACK_PLATFORMS_CI}", "#platforms_ci",
+              sendNotification("Build", "${env.SLACK_PLATFORMS_CI}", "#newsroom-eng-releases",
                 """
                   ${sbtFolder}/sbt clean test
                 """
@@ -99,7 +99,7 @@ ansiColor('xterm') {
                 }
             }
             stage("PreProd Deploy"){
-              sendNotification("Deploy PreProd", "${env.SLACK_PLATFORMS_RELEASES}", "#platforms_releases",
+              sendNotification("Deploy PreProd", "${env.SLACK_PLATFORMS_RELEASES}", "#newsroom-eng-releases",
                 """
                   ${sbtFolder}/sbt preprod:stackSetup
                 """
@@ -124,11 +124,11 @@ ansiColor('xterm') {
             }
 
             stage("Validate prod deploy") {
-                timeout(time: 2, unit: 'MINUTES') {
+                timeout(time: 30, unit: 'MINUTES') {
 
                     current_commit = sh(returnStdout: true, script: 'git show-ref --tags --head --hash| head -n1').trim()
                     previous_release_tag = sh(returnStdout: true, script: 'git show-ref --tags --head | sort -V -k2,2 | tail -n1 | cut -d " " -f1').trim()
-                    tickets = sh(returnStdout: true, script: """git log --full-diff $previous_release_tag..$current_commit | grep -o 'PLAT-[0-9]*'| sort -u | uniq |awk '{print "https://jira.aws.telegraph.co.uk/browse/"\$1}'""").trim()
+                    tickets = sh(returnStdout: true, script: """git log --full-diff $previous_release_tag..$current_commit | grep -o 'NF-[0-9]*'| sort -u | uniq |awk '{print "https://jira.aws.telegraph.co.uk/browse/"\$1}'""").trim()
                     authors = sh(returnStdout: true, script: """git log --full-diff $previous_release_tag..$current_commit | grep -o 'Author: .*' | sed -e 's/Author: //g' | sort -u | uniq""").trim()
 
                     sendMessage(
@@ -145,29 +145,29 @@ ansiColor('xterm') {
                 }
             }
 
-//            stage("Prod Deploy"){
-//              sendNotification("Deploy Prod", "${env.SLACK_PLATFORMS_RELEASES}", "#platforms_releases",
-//                """
-//                  ${sbtFolder}/sbt prod:stackSetup
-//                """
-//              )
-//            }
-//
-//            stage("Release Notes"){
-//                // Possible error if there is a commit different from the trigger commit
-//                github_commit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-//
-//                //Realease on Git
-//                println("\n[TRACE] **** Releasing to github ${github_token}, ${pipeline_version}, ${github_commit} ****")
-//                sh """#!/bin/bash
-//                    GITHUB_COMMIT_MSG=\$(curl -H "Content-Type: application/json" -H "Authorization: token ${github_token}" https://api.github.com/repos/telegraph/${projectName}/commits/\"${github_commit}\" | /usr/local/bin/jq \'.commit.message\')
-//                    echo "GITHUB_COMMIT_MSG: \${GITHUB_COMMIT_MSG}"
-//                    echo "GITHUB_COMMIT_DONE: DONE"
-//                    C_DATA="{\\\"tag_name\\\": \\\"${pipeline_version}\\\",\\\"target_commitish\\\": \\\"master\\\",\\\"name\\\": \\\"${pipeline_version}\\\",\\\"body\\\": \${GITHUB_COMMIT_MSG},\\\"draft\\\": false,\\\"prerelease\\\": false}"
-//                    echo "C_DATA: \${C_DATA}"
-//                    curl -H "Content-Type: application/json" -H "Authorization: token ${github_token}" -X POST -d "\${C_DATA}" https://api.github.com/repos/telegraph/${projectName}/releases
-//                """
-//            }
+            stage("Prod Deploy"){
+              sendNotification("Deploy Prod", "${env.SLACK_PLATFORMS_RELEASES}", "#newsroom-eng-releases",
+                """
+                  ${sbtFolder}/sbt prod:stackSetup
+                """
+              )
+            }
+
+            stage("Release Notes"){
+                // Possible error if there is a commit different from the trigger commit
+                github_commit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+
+                //Realease on Git
+                println("\n[TRACE] **** Releasing to github ${github_token}, ${pipeline_version}, ${github_commit} ****")
+                sh """#!/bin/bash
+                    GITHUB_COMMIT_MSG=\$(curl -H "Content-Type: application/json" -H "Authorization: token ${github_token}" https://api.github.com/repos/telegraph/${projectName}/commits/\"${github_commit}\" | /usr/local/bin/jq \'.commit.message\')
+                    echo "GITHUB_COMMIT_MSG: \${GITHUB_COMMIT_MSG}"
+                    echo "GITHUB_COMMIT_DONE: DONE"
+                    C_DATA="{\\\"tag_name\\\": \\\"${pipeline_version}\\\",\\\"target_commitish\\\": \\\"master\\\",\\\"name\\\": \\\"${pipeline_version}\\\",\\\"body\\\": \${GITHUB_COMMIT_MSG},\\\"draft\\\": false,\\\"prerelease\\\": false}"
+                    echo "C_DATA: \${C_DATA}"
+                    curl -H "Content-Type: application/json" -H "Authorization: token ${github_token}" -X POST -d "\${C_DATA}" https://api.github.com/repos/telegraph/${projectName}/releases
+                """
+            }
         }
     }
 }
